abb6d49fdd12b9c19ddc7e195c5f5f6033f1131e
Karol Ciesluk <karolciesluk.db@gmail.com>
2022-01-30 12:54:04 +0100

    add display name to withUser hoc

--- src/components/withUsers.tsx
+++ src/components/withUsers.tsx
@@ -1,12 +1,18 @@
 import React, { useContext } from 'react';
 import { User, UserContext } from '../context/userContext';
 
+const getDisplayName = (WrappedComponent: React.ComponentType<User>) => {
+  return WrappedComponent.displayName || WrappedComponent.name || 'Component';
+};
+
 const withUser = (WrappedComponent: React.ComponentType<User>) => {
   const ComponentWithUser = () => {
     const user = useContext(UserContext);
 
     return <WrappedComponent email={user.email} isAdmin={user.isAdmin} />;
   };
+
+  ComponentWithUser.displayName = `WithUser(${getDisplayName(WrappedComponent)})`;
 
   return ComponentWithUser;
 };
