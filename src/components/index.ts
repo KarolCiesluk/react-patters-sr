8815263546e72da9b5a96098e3a7f6b5c5cbdf76
Karol Ciesluk <karolciesluk.db@gmail.com>
2022-01-30 12:41:22 +0100

    export Movies and Places from components folder

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,4 @@
 export { default as Content } from "./Content";
 export { default as PassphraseForm } from "./PassphraseForm";
+export { default as Movies } from "./Movies";
+export { default as Places } from "./Places";
