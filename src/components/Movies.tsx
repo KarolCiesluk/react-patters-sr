f0c0852fb8e42571509dade3e718ceb4f1cdc602
Karol Ciesluk <karolciesluk.db@gmail.com>
2022-01-31 10:53:03 +0100

    add LoginChecker to Movies and Places

--- src/components/Movies.tsx
+++ src/components/Movies.tsx
@@ -1,13 +1,14 @@
+import { LoginChecker } from ".";
 import { User } from "../context/userContext";
 import withUser from "./withUsers";
 
 const Movies = ({ email, isAdmin }: User) => {
   return (
-    <>
+    <LoginChecker>
       <h1>Movies</h1>
       <p>User email: {email}</p>
       <p>Admin: {isAdmin.toString()}</p>
-    </>
+    </LoginChecker>
   );
 };
 
--- src/components/Places.tsx
+++ src/components/Places.tsx
@@ -1,12 +1,13 @@
+import { LoginChecker } from ".";
 import { User } from "../context/userContext";
 import withUser from "./withUsers";
 
 const Places = ({ email, isAdmin }: User) => (
-  <>
+  <LoginChecker>
     <h1>Places</h1>
     <p>User email: {email}</p>
     <p>Admin: {isAdmin.toString()}</p>
-  </>
+  </LoginChecker>
 );
 
 export default withUser(Places);
