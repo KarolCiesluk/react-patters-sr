692f12575410f12afb1e7deb5ccf1294b3bd2183
Karol Ciesluk <karolciesluk.db@gmail.com>
2022-01-31 10:51:53 +0100

    move index styling to styles folder

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,6 @@
-export { default as Content } from "./Content";
+export { default as LoginChecker } from "./LoginChecker";
 export { default as PassphraseForm } from "./PassphraseForm";
 export { default as Movies } from "./Movies";
 export { default as Places } from "./Places";
+export { default as Menu } from "./Menu";
+export { default as Content } from "./Content";
