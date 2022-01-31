ecbae3cb1fd11b4cd7296f4c7ce3a86009fab45f
Karol Ciesluk <karolciesluk.db@gmail.com>
2022-01-31 10:54:09 +0100

    update App and index

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,52 @@
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 
-import { Content, Movies, PassphraseForm, Places } from "./components";
+import { Content, Menu, Movies, PassphraseForm, Places } from "./components";
+import NavItem from "./components/NavItem";
 import { LoginContextValue } from "./context/loginContext";
 import { UserContext } from "./context/userContext";
+import { toContent, toMovies, toPlaces } from "./routes";
+import "./styles/App.css";
 
 function App() {
   return (
     <Router>
-      <LoginContextValue>
-        <UserContext.Provider
-          value={{
-            email: "jan@kowalski.pl",
-            isAdmin: false,
-          }}
-        >
-          <Routes>
-            <Route index element={<PassphraseForm />} />
-            <Route path="/content" element={<Content />} />
-            <Route path="/movies" element={<Movies />} />
-            <Route path="/places" element={<Places />} />
-          </Routes>
-        </UserContext.Provider>
-      </LoginContextValue>
-    </Router>
+      <div className="app">
+        <LoginContextValue>
+
+          <Menu>
+            {isMobile => {
+              const variant = isMobile ? "mobile" : "desktop";
+
+              return (
+                <>
+                  <NavItem to={toContent()} label="Content" variant={variant} />
+                  <NavItem to={toMovies()} label="Movies" variant={variant} />
+                  <NavItem to={toPlaces()} label="Places" variant={variant} />
+                </>
+              );
+            }}
+          </Menu>
+
+          <main className="app_main">
+            <UserContext.Provider
+              value={{
+                email: "jan@kowalski.pl",
+                isAdmin: false,
+              }}
+            >
+              <Routes>
+                <Route index element={<PassphraseForm />} />
+                <Route path={toContent()} element={<Content />} />
+                <Route path={toMovies()} element={<Movies />} />
+                <Route path={toPlaces()} element={<Places />} />
+              </Routes>
+            </UserContext.Provider>
+
+          </main>
+
+        </LoginContextValue>
+      </div>
+    </Router >
   );
 }
 
